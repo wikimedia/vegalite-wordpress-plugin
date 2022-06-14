afe1ecf9b97d5bbc19774efeb54e16a9375d9be3
Sean McCafferty <sean@mccafferty.co>
2022-06-14 09:05:39 -0400

    Rework the datavis embed to be more efficient
    Add multiple checks to prevent JS errors, add a shortcircuit to prevent infinite loops

--- src/blocks/datavis-block/EditDatavisBlock.js
+++ src/blocks/datavis-block/EditDatavisBlock.js
@@ -64,33 +64,42 @@ const EditDatavisBlock = ( { attributes, setAttributes } ) => {
 };
 
 /**
- * Hacky way to apply an update to the Datavis model.
+ * Hacky way to get the Datavis model to render after updates have been made to the block.
  *
  * This is needed due to the lack of an action when the Server Side render is completed. So we create a interval
  * to check if the block element has been updated, if not it run the Datavis setup.
  *
- * @param blockId
- * @constructor
+ * @param {string} blockId Block id.
+ * @class
  */
 const PreviewDatavis = ( blockId ) => {
-	let dataVisLoaded = false;
-	let dataVisLoadedInterval = setInterval(function() {
+	let dataVisLoaded = false,
+		killCounter = 0;
+	let dataVisLoadedInterval = setInterval( function() {
 		const element = document.getElementById( blockId+'-datavis' );
 		if ( ! element ) {
 			return;
 		}
-
 		// To prevent multiple reloads, only run preview if the block element has loaded and the Datavis has not been loaded.
-		const loaded = element.dataset.loaded ?? false;
+		let loadedElement = element.dataset.loaded ?? false,
+			loaded = ( loadedElement === 'true' );
+
 		if ( element && ! loaded ) {
+			// Element is present but the dataviz configuration has not yet rendered from the server.
 			dataVisLoaded = true;
 			element.setAttribute( 'data-loaded', true );
 		}
-		if ( dataVisLoaded && !! loaded ) {
+		// Element exists and the dataviz configuration has rendered from the server so kick off the processing of the dataviz configuration.
+		if ( dataVisLoaded && loaded ) {
 			setupDatavisBlocks();
 			clearInterval( dataVisLoadedInterval );
 		}
-	}, 1000);
+		// If after a period of time kill the interval to not fall into an infinite loop.
+		if ( killCounter > 10 ) {
+			clearInterval( dataVisLoadedInterval );
+		}
+		killCounter += 1;
+	}, 1000 );
 };
 
 export default EditDatavisBlock;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 /**
+ * Front-end functionality for datavis blocks.
+ */
+
+/**
  * A collection of all datavis blocks on the page.
  *
  * @type {Element[]}
@@ -6,17 +10,44 @@
  */
 let _instances = [];
 
+/**
+ * Entry function to initialize all datavis blocks to render the blocks datavis model.
+ */
 export function setupDatavisBlocks() {
 	// Get all datavis block ids.
 	_instances = [ ...document.querySelectorAll( '[data-datavis]' ) ];
 	_instances.map( initializeDatavisBlock );
 }
 
+/**
+ * Callback to initialize a datavis block to render its model.
+ *
+ * @param {Element} element Datavis block element.
+ */
 function initializeDatavisBlock( element ) {
-	const json = document.getElementById( element.dataset.config ).textContent;
-	vegaEmbed( '#'+element.dataset.datavis, JSON.parse( json ) );
+	const config = element.dataset.config,
+		datavis = element.dataset.datavis;
+
+	if ( typeof vegaEmbed === 'undefined' ) {
+		return;
+	}
+
+	if ( ! config || ! datavis ) {
+		return;
+	}
+
+	const jsonElement = document.getElementById( config );
+	if ( ! jsonElement ) {
+		return;
+	}
+
+	/* eslint-disable-next-line no-undef */
+	vegaEmbed( '#'+element.dataset.datavis, JSON.parse( jsonElement.textContent ) );
 }
 
+/**
+ * Kick things off after load.
+ */
 window.onload = function() {
 	setupDatavisBlocks();
 };
