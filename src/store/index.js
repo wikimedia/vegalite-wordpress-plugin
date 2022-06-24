c3744b6f45765f1efe09d1213e6d778b8fe08ba8
K. Adam White <kadamwhite@users.noreply.github.com>
2022-06-24 17:47:29 +0100

    Continue implementing dataset store

--- src/store/index.js
+++ src/store/index.js
@@ -3,110 +3,240 @@ import { createSelector } from 'reselect';
 
 import { createReduxStore, register } from '@wordpress/data';
 
-import { getDatasets } from '../util/datasets';
+import * as api from '../util/datasets';
 
-const DEFAULT_STATE = {
-	datasets: {},
-	postType: '',
-	postId: 0,
-};
-
-/*
-select( 'datasets' ).getDatasets();
-
-select( 'datasets' ).getDatasetByUrl( '...' );
-select( 'datasets' ).getDatasetByFilename(  );
-*/
-
+/* eslint-disable jsdoc/require-returns-description */
 const actions = {
-	setPost: ( id, type ) => ( {
-		type: 'POST_SET',
-		post: {
-			id,
-			type,
-		},
-	} ),
-
+	/**
+	 * @param {Dataset} dataset Dataset to save.
+	 * @returns {ReduxAction}
+	 */
 	createDataset: ( dataset ) => ( {
 		type: 'DATASET_CREATE',
 		dataset,
 	} ),
 
+	/**
+	 * @param {Dataset} dataset Dataset to update.
+	 * @returns {ReduxAction}
+	 */
 	updateDataset: ( dataset ) => ( {
 		type: 'DATASET_UPDATE',
 		dataset,
 	} ),
 
+	/**
+	 * @param {Dataset} dataset Dataset to delete.
+	 * @returns {ReduxAction}
+	 */
 	deleteDataset: ( dataset ) => ( {
 		type: 'DATASET_DELETE',
 		dataset,
 	} ),
 
+	/**
+	 * @param {string} filename Filename of dataset to retrieve.
+	 * @returns {ReduxAction}
+	 */
 	getDataset: ( filename ) => ( {
 		type: 'DATASET_GET',
 		filename,
 	} ),
 
+	/**
+	 * @param {string} url Full URL of dataset to retrieve.
+	 * @returns {ReduxAction}
+	 */
 	getDatasetByUrl: ( url ) => ( {
 		type: 'DATASET_GET_BY_URL',
 		url,
 	} ),
 
+	/**
+	 * @returns {ReduxAction}
+	 */
+	getDatasets: () => ( {
+		type: 'DATASETS_GET_ALL',
+	} ),
+
+	/**
+	 * @param {Dataset[]} datasets Array of datasets to save in store.
+	 * @returns {ReduxAction}
+	 */
 	setDatasets: ( datasets ) => ( {
 		type: 'DATASETS_SET',
 		datasets,
 	} ),
 
-	fetchDatasets: () => ( {
-		type: 'DATASET_FETCH_ALL',
+	/**
+	 * @param {Dataset} dataset Dataset to save in store.
+	 * @returns {ReduxAction}
+	 */
+	setDataset: ( dataset ) => ( {
+		type: 'DATASET_SET',
+		dataset,
+	} ),
+
+	/**
+	 * @param {Dataset} dataset Dataset to remove from store.
+	 * @returns {ReduxAction}
+	 */
+	unsetDataset: ( dataset ) => ( {
+		type: 'DATASET_UNSET',
+		dataset,
 	} ),
 };
+/* eslint-enable jsdoc/require-returns-description */
 
 const controls = {
-	DATASET_FETCH_ALL: () => getDatasets(),
+	/* eslint-disable jsdoc/require-returns */
+	DATASETS_GET_ALL: () => api.getDatasets(),
+	/** @param {ReduxAction} action Dispatched action */
+	DATASET_GET: ( { filename } ) => api.getDataset( filename ),
+	/** @param {ReduxAction} action Dispatched action */
+	DATASET_GET_BY_URL: ( { url } ) => api.getDatasetByUrl( url ),
+	/** @param {ReduxAction} action Dispatched action */
+	DATASET_CREATE: ( { dataset } ) => api.createDataset( dataset ),
+	/** @param {ReduxAction} action Dispatched action */
+	DATASET_UPDATE: ( { dataset } ) => api.updateDataset( dataset ),
+	/** @param {ReduxAction} action Dispatched action */
+	DATASET_DELETE: ( { dataset } ) => api.deleteDataset( dataset ),
+	/* eslint-enable jsdoc/require-returns */
 };
 
+/* eslint-disable jsdoc/require-returns */
 const resolvers = {
 	*getDatasets() {
-		const datasets = yield actions.fetchDatasets();
+		/** @type {Dataset[]} */
+		const datasets = yield actions.getDatasets();
 		return actions.setDatasets( datasets );
 	},
-}
-
-/* eslint-disable default-case */
-const store = createReduxStore( 'csv-datasets', {
 	/**
-	 * Store reducer.
-	 *
-	 * @param {object} state  State tree
-	 * @param {object} action Action object.
-	 * @returns {object} Transformed state tree.
+	 * @param {string} filename Filename of a dataset to retrieve.
 	 */
-	reducer( state = DEFAULT_STATE, action ) {
-		switch ( action.type ) {
-			case 'POST_SET':
-				return {
-					...state,
-					postType: action.post.type,
-					postId: action.post.id,
-				};
-			case 'DATASETS_SET':
-				return {
-					...state,
-					datasets: {
-						...action.datasets.reduce(
-							( memo, dataset ) => ( {
-								...memo,
-								[ dataset.filename ]: dataset,
-							} ),
-							{}
-						),
-					},
-				};
-		}
-
-		return state;
+	*getDataset( filename ) {
+		/** @type {Dataset} */
+		const dataset = yield actions.getDataset( filename );
+		return actions.setDataset( dataset );
 	},
+	/**
+	 * @param {string} url Public URL of a dataset to retrieve.
+	 */
+	*getDatasetByUrl( url ) {
+		/** @type {Dataset} */
+		const dataset = yield actions.getDatasetByUrl( url );
+		return actions.setDataset( dataset );
+	},
+	/**
+	 * @param {Dataset} dataset Dataset to create.
+	 */
+	*createDataset( dataset ) {
+		/** @type {Dataset} */
+		const savedDataset = yield actions.createDataset( dataset );
+		return actions.setDataset( savedDataset );
+	},
+	/**
+	 * @param {Dataset} dataset Dataset to update.
+	 */
+	*updateDataset( dataset ) {
+		/** @type {Dataset} */
+		const savedDataset = yield actions.updateDataset( dataset );
+		return actions.setDataset( savedDataset );
+	},
+	/**
+	 * @param {Dataset} dataset Dataset to delete.
+	 */
+	*deleteDataset( dataset ) {
+		/** @type {bool} */
+		const didDelete = yield actions.deleteDataset( dataset );
+		if ( didDelete ) {
+			return actions.unsetDataset( dataset );
+		}
+	},
+};
+/* eslint-enable jsdoc/require-returns */
+
+/**
+ * Redux dataset store.
+ *
+ * @typedef DatasetStore
+ * @property {object.<string, Dataset>} datasets Dataset dictionary keyed by filename.
+ */
+
+/** @type {DatasetStore} */
+const DEFAULT_STATE = {
+	datasets: {},
+};
+
+/**
+ * Store reducer.
+ *
+ * @param {DatasetStore} state  State tree
+ * @param {object} action Action object.
+ * @returns {object} Transformed state tree.
+ */
+const reducer = ( state = DEFAULT_STATE, action ) => {
+	/* eslint-disable default-case */
+	switch ( action.type ) {
+		case 'DATASETS_SET':
+			return {
+				...state,
+				datasets: {
+					...action.datasets.reduce(
+						( memo, dataset ) => ( {
+							...memo,
+							[ dataset.filename ]: dataset,
+						} ),
+						{}
+					),
+				},
+			};
+		case 'DATASET_SET':
+			if ( ! action.dataset?.filename ) {
+				return state;
+			}
+			return {
+				...state,
+				datasets: {
+					...state.datasets,
+					[ action.dataset.filename ]: {
+						...( state.datasets[ action.dataset.filename ] || {} ),
+						...action.dataset,
+					},
+				},
+			};
+		case 'DATASET_UPDATE':
+			if ( ! action.dataset?.filename ) {
+				return state;
+			}
+			return {
+				...state,
+				datasets: {
+					...state.datasets,
+					[ action.dataset.filename ]: {
+						...( state.datasets[ action.dataset.filename ] || {} ),
+						...action.dataset,
+					},
+				},
+			};
+		case 'DATASET_DELETE':
+			if ( ! action.dataset?.filename ) {
+				return state;
+			}
+			return {
+				...state,
+				datasets: {
+					...state.datasets,
+					[ action.dataset.filename ]: undefined,
+				},
+			};
+	}
+
+	return state;
+};
+
+const store = createReduxStore( 'csv-datasets', {
+	reducer,
 
 	actions,
 
@@ -114,30 +244,36 @@ const store = createReduxStore( 'csv-datasets', {
 		/**
 		 * Get array of datasets.
 		 *
-		 * @param {object} state State tree.
-		 * @returns {object[]} Array of datasets.
+		 * @param {DatasetStore} state State tree.
+		 * @returns {Dataset[]} Array of datasets.
 		 */
 		getDatasets: createSelector(
 			( state ) => state.datasets,
-			( datasets ) => Object.values( datasets )
+			( datasets ) => Object.values( datasets ).filter( Boolean )
 		),
 
 		/**
 		 * Retrieve a dataset by filename string.
 		 *
-		 * @param {object} state State tree.
-		 * @param {string} url   Dataset public URL.
-		 * @returns {object} Dataset object.
+		 * @param {DatasetStore} state    State tree.
+		 * @param {string}       filename Filename of requested dataset..
+		 * @returns {?Dataset} Dataset object, or null if not found.
 		 */
-		getDatasetByFilename( state, url ) {
-			return {};
+		getDataset( state, filename ) {
+			return state.datasets[ filename ] || null;
 		},
-		getDatasetByUrl( state, url ) {},
-		getPrice( state, item ) {
-			const { prices, discountPercent } = state;
-			const price = prices[ item ];
 
-			return price * ( 1 - 0.01 * discountPercent );
+		/**
+		 * Retrieve a dataset by its public URL.
+		 *
+		 * @param {DatasetStore} state State tree.
+		 * @param {string}       url   Dataset public URL.
+		 * @returns {?Dataset} Dataset object, or null if not found.
+		 */
+		getDatasetByUrl( state, url ) {
+			/** @type {Dataset[]} */
+			const datasets = Object.values( state.datasets );
+			return datasets.find( ( dataset ) => dataset.url === url ) || null;
 		},
 	},
 
