99f60fcca8bb97873f02dda74cf692a073f8a20c
K. Adam White <kadamwhite@users.noreply.github.com>
2026-08-04 18:21:29 -0400

    Add jest unit test framework
    Even when using wp-scripts, We have to specify a little more configuration than usual since we want to add some methods from the Browser context which are not defined in Node by default

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const path = require( 'path' );
+
+module.exports = {
+	'preset': '@wordpress/jest-preset-default',
+
+	// Stand on top of the Babel config wp-scripts would use to generate Jest config.
+	'transform': {
+		'\\.[jt]sx?$': path.join(
+			path.dirname( require.resolve( '@wordpress/scripts/package.json' ) ),
+			'config/babel-transform'
+		),
+	},
+
+	// Merged with, not replacing, the preset's own setupFiles.
+	'setupFiles': [ '<rootDir>/jest.setup.js' ],
+};
